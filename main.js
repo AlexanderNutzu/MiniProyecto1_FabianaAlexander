c533712bae0b01c78ad571d1df6da5ed9bae8332
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2025-02-13 19:21:50 -0400

    JS inicialización de variables y llamados html

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 /*Empezando archivos*/
+
+const colors = ["red", "green", "blue", "yellow"];
+let sequence = [];
+let playerSequence = [];
+let level = 1;
+let isGameActive = false;
+
+
+let jugador= null;
+
+
+const startBtn = document.getElementById("start-btn");
+const levelDisplay = document.getElementById("level");
+const colorButtons = document.querySelectorAll(".color-btn");
+const scoreTableBody = document.querySelector("#score-table tbody");
